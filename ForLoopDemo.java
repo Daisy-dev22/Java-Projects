535b5daa43dae63d12de36e4e26b60d900ee9757
Daisy-dev22 <kachivembadaisy@gmail.com>
2026-04-27 21:02:25 +0200

    Just java practice codes

---
+++ ForLoopDemo.java
@@ -0,0 +1,13 @@
+/*
+ * Name: ForLoopDemo.java
+ *  
+ */
+public class ForLoopDemo {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Loop iteration: " + i);
+        }
+    }
+
+}
---
+++ WhileDemo.java
@@ -0,0 +1,10 @@
+public class WhileDemo {
+    public static void main(String[] args) {
+        int count = 1;
+        while (true) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+
+}
